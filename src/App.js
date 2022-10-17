4708ed77b70dd5118affc68dd529fe81ee62c08f
Ty Menlove <tjmundecided@gmail.com>
2022-10-17 13:20:16 -0600

    deleted extra html inside of app.js

--- src/App.js
+++ src/App.js
@@ -5,23 +5,7 @@ import Header from './components/Header'
 function App() {
   return (
     <div className="App">
-        <div className="App">
           <Header/>
-        </div>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
